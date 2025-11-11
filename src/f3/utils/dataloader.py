933799f36da27a26d594f1714be6e77d61fca444
Lim Tian Yi <ltianyi@dso.org.sg>
2025-11-11 11:22:45 +0800

    Guess m3ed is not zero-aligned, but "sufficiently small" should be ok...

--- src/f3/utils/dataloader.py
+++ src/f3/utils/dataloader.py
@@ -129,7 +129,7 @@ class BaseExtractor(Dataset):
         assert isinstance(self.events_y, h5py.Dataset)
         assert isinstance(self.events_t, h5py.Dataset)
         assert isinstance(self.events_p, h5py.Dataset)
-        assert self.events_t[0] == 0, "First timestamp is not zero. This breaks some of the assumptions in this repo."
+        assert self.events_t[0] < time_ctx, f"First timestamp {self.events_t[0]}>={time_ctx}. This breaks some of the assumptions in this repo."
 
         self.dtype = dtype
         self.camera = camera
